1fd29398729b6b4eb96374ecebcffb8236af572b
middmoon <middmoon.68@gmail.com>
2025-04-16 08:15:47 +0700

    recover 3

--- client/src/components/Listcard/ProductList.jsx
+++ client/src/components/Listcard/ProductList.jsx
@@ -10,9 +10,7 @@ const ProductList = () => {
       try {
         const response = await getProduct();
         if (response.status === 200) {
-          const shuffledProducts = response.data.sort(
-            () => Math.random() - 0.5
-          );
+          const shuffledProducts = response.data.sort(() => Math.random() - 0.5);
           setProducts(shuffledProducts);
         }
       } catch (error) {
@@ -26,9 +24,7 @@ const ProductList = () => {
   const getDirectImageURL = (url) => {
     if (!url) return "/Img/Page/p3.webp";
     const match = url.match(/(?:id=|\/d\/)([^/&?]+)/);
-    return match
-      ? `https://lh3.googleusercontent.com/d/${match[1]}=s1000`
-      : url;
+    return match ? `https://lh3.googleusercontent.com/d/${match[1]}=s1000` : url;
   };
 
   return (
@@ -39,21 +35,16 @@ const ProductList = () => {
 
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-20 pb-10 relative">
         {products.slice(0, 12).map((product) => {
-          const avatarImage = getDirectImageURL(
-            product.ProductImages?.find((img) => img.is_avatar)?.img_url ||
-              product.ProductImages?.[0]?.img_url
-          );
+          const avatarImage = getDirectImageURL(product.ProductImages?.find((img) => img.is_avatar)?.img_url || product.ProductImages?.[0]?.img_url);
 
           return (
             <ProductCard
               key={product._id}
               image={avatarImage}
               name={product.name}
-              price={
-                product.retail_price
-                  ? `${product.retail_price.toLocaleString("vi-VN")} VND`
-                  : "Liên hệ"
-              }
+              price={product.retail_price}
+              isSale={product.Events.length > 0}
+              sale_price={product.Events[0]?.discount_value}
               link={`/productdetail/${product.slug}`}
             />
           );
